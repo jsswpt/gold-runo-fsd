24be0f3116bdf81fb9381b6bd477ca239b422a47
jsswpt <paiusov.d@yandex.ru>
2023-02-09 16:11:32 +0500

    Шапка

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -5,25 +5,28 @@ import cn from "classnames";
 import { Container } from "@/shared/ui/container/container";
 import Image from "next/image";
 
-import Logo from "shared/assets/media/imgs/logo.png";
 import Link from "next/link";
 import { useScreen } from "@/shared/hooks";
 
 import { motion, useAnimationControls } from "framer-motion";
+import { Dropdown, Modal } from "@/shared/ui";
+import { Logo } from "@/shared/assets";
+import { SearchFurniture } from "@/features";
 
+/**Нужно отрефакторить:
+ * сделать animatePresence, перенести bottom в ui
+ */
 export const Header = () => {
   const [isScrolledEnough, setIsScrolledEnough] = useState(false);
 
   const { currentScreen } = useScreen();
 
-  const topBlockControls = useAnimationControls();
   const headerControls = useAnimationControls();
   const bottomBlockControls = useAnimationControls();
   const bottomBlockInnerControls = useAnimationControls();
 
-  const infoBlockRef = useRef<HTMLDivElement>(null);
-  const bottomBlockRef = useRef<HTMLDivElement>(null);
   const headerRef = useRef<HTMLElement>(null);
+  const bottomBlockRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -37,21 +40,15 @@ export const Header = () => {
 
   useEffect(() => {
     if (isScrolledEnough) {
-      headerControls.start({ y: `-${infoBlockRef.current?.clientHeight}px` });
       bottomBlockControls.start({
-        y: `-${
-          bottomBlockRef.current!.clientHeight +
-          infoBlockRef.current!.clientHeight
-        }px`,
+        y: `-${bottomBlockRef.current!.clientHeight}px`,
       });
       bottomBlockInnerControls.start({
         y: `-${bottomBlockRef.current!.clientHeight}px`,
       });
     } else if (!isScrolledEnough) {
       headerControls.start({ y: `0` });
-      topBlockControls.start({ opacity: 1 });
       bottomBlockControls.start({
-        opacity: 1,
         y: 0,
       });
       bottomBlockInnerControls.start({ y: "0" });
@@ -59,78 +56,90 @@ export const Header = () => {
   }, [isScrolledEnough]);
 
   return (
-    <div className={st.top_wrapper}>
-      <div className={st.top_wrapper__relative_wrapper}>
-        <motion.div
-          ref={infoBlockRef}
-          animate={topBlockControls}
-          className={st.info_block}
-        >
-          <Container className={st.info_container}>Важная инфа</Container>
-        </motion.div>
-        <motion.header
-          ref={headerRef}
-          animate={headerControls}
-          transition={{ duration: 0.32, mass: 2 }}
-          className={st.header}
-        >
-          <Container className={st.header_container}>
-            <div className={cn(st.header_wrap, st.header_wrap__logo)}>
-              <Link href="/" className={st.logo_img_wrapper}></Link>
-            </div>
-            <nav className={cn(st.header_wrap, st.header_wrap__nav)}>
-              Навигация
-            </nav>
-            <div className={cn(st.header_wrap, st.header_wrap__search)}>
-              Поиск
-            </div>
-            <div className={cn(st.header_wrap, st.header_wrap__info)}>Инфа</div>
-            <div className={cn(st.header_wrap, st.header_wrap__actions)}>
-              Экшоны
-            </div>
-          </Container>
-        </motion.header>
-        <motion.div
-          animate={bottomBlockControls}
-          transition={{ delay: 0, duration: 0.32 }}
-          ref={bottomBlockRef}
-          className={st.bottom_block}
-        >
-          <Container className={st.bottom_container}>
-            <motion.div
-              animate={bottomBlockInnerControls}
-              transition={{ delay: 0.32, duration: 0.56 }}
-              className={st.bottom_wrap}
-            >
-              ??? |
-            </motion.div>
-            <motion.div
-              animate={bottomBlockInnerControls}
-              transition={{ delay: 0.32, duration: 0.56 }}
-              className={st.bottom_wrap}
-            >
-              {" "}
-              ??? |
-            </motion.div>
-            <motion.div
-              animate={bottomBlockInnerControls}
-              transition={{ delay: 0.32, duration: 0.56 }}
-              className={st.bottom_wrap}
-            >
-              {" "}
-              ??? |
-            </motion.div>
-            <motion.div
-              animate={bottomBlockInnerControls}
-              transition={{ delay: 0.32, duration: 0.56 }}
-              className={st.bottom_wrap}
-            >
-              {" "}
-              ???
-            </motion.div>
-          </Container>
-        </motion.div>
-      </div>
-    </div>
+    <>
+      <motion.header
+        ref={headerRef}
+        animate={headerControls}
+        transition={{ duration: 0.24, mass: 2 }}
+        className={st.header}
+      >
+        <Container className={st.header_container}>
+          <div className={cn(st.header_wrap, st.header_wrap__logo)}>
+            <Link href="/" className={st.logo_img_wrapper}>
+              <Logo />
+            </Link>
+          </div>
+          <nav className={cn(st.header_wrap, st.header_wrap__nav)}>
+            <ul className={st.header_nav__list}>
+              <li className={st.header_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  Товары
+                </Link>
+              </li>
+              <li className={st.header_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  Интерьеры
+                </Link>
+              </li>
+              <li className={st.header_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  Доставка и оплата
+                </Link>
+              </li>
+              <li className={st.header_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  О компании
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={cn(st.header_wrap, st.header_wrap__search)}>
+            <SearchFurniture />
+          </div>
+          <div className={cn(st.header_wrap, st.header_wrap__info)}>Инфа</div>
+          <div className={cn(st.header_wrap, st.header_wrap__actions)}>
+            Экшоны
+          </div>
+        </Container>
+      </motion.header>
+      <motion.div
+        ref={bottomBlockRef}
+        animate={bottomBlockControls}
+        transition={{ delay: 0, duration: 0.24, mass: 2 }}
+        ref={bottomBlockRef}
+        className={st.bottom_block}
+      >
+        <Container className={st.bottom_container}>
+          <motion.nav
+            animate={bottomBlockInnerControls}
+            transition={{ duration: 0.4, delay: 0.24 }}
+            className={cn(st.bottom_wrap, st.bottom_wrap__nav)}
+          >
+            <ul className={st.bottom_nav__list}>
+              <li className={st.bottom_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  Товары
+                </Link>
+              </li>
+              <li className={st.bottom_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  Интерьер
+                </Link>
+              </li>
+              <li className={st.bottom_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  Доставка и оплата
+                </Link>
+              </li>
+              <li className={st.bottom_nav__item}>
+                <Link href="" className={cn("link1", st.header_nav__link)}>
+                  О компании
+                </Link>
+              </li>
+            </ul>
+          </motion.nav>
+        </Container>
+      </motion.div>
+    </>
   );
 };
