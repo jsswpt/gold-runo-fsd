a705d4f5f620a9c5624c04c8293fcee0c562c223
jsswpt <paiusov.d@yandex.ru>
2023-02-21 19:13:50 +0500

    перереботка

---
+++ src/widgets/product-card/swiper-desktop/swiper-desktop.tsx
@@ -0,0 +1,11 @@
+import {} from "react";
+
+import st from "./styles.module.scss";
+import cn from "classnames";
+import { SwiperProps } from "../product-card.type";
+
+interface SwiperDesktop extends SwiperProps {}
+
+export const SwiperDesktop = (props: SwiperDesktop) => {
+  return <>SwiperDesktop</>;
+};
