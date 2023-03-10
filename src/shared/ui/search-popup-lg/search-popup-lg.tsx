013eb1d7d6fdd4627d93db1f97f6966702af0384
jsswpt <paiusov.d@yandex.ru>
2023-03-10 14:21:16 +0500

    Какие-то фичи и декомпозиция

---
+++ src/shared/ui/search-popup-lg/search-popup-lg.tsx
@@ -0,0 +1,18 @@
+import {} from "react";
+
+import st from "./styles.module.scss";
+import cn from "classnames";
+import { AnimatePresence, motion } from "framer-motion";
+import { Container } from "../container/container";
+import { Divider } from "../divider/divider";
+
+type SearchPopupLg = {
+  isFocused: boolean;
+  children: React.ReactNode;
+};
+
+export const SearchPopupLg = (props: SearchPopupLg) => {
+  return (
+    
+  );
+};
