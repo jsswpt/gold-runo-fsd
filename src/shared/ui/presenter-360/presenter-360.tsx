4c5414a60eaf53dd3477c4adf22b5e3a22b9939c
jsswpt <paiusov.d@yandex.ru>
2023-03-13 18:55:56 +0500

    доделать слайдер

--- src/shared/ui/presenter-360/presenter-360.tsx
+++ src/shared/ui/presenter-360/presenter-360.tsx
@@ -8,60 +8,67 @@ type Presenter360 = {
   className?: any;
 };
 
-type Direction = "horizontal" | "vertical";
-
-type Pos = {
-  start: number;
-  end: number;
-};
-
 export const Presenter360 = (props: Presenter360) => {
-  const [isHolding, setIsHolding] = useState(false);
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [startX, setStartX] = useState(0);
-
   const wrapperRef = useRef<HTMLDivElement>(null);
   const sliderWrapRef = useRef<HTMLDivElement>(null);
 
-  const startMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    setIsHolding(true);
-    setStartX(e.nativeEvent.offsetX);
-  };
+  const [isHolding, setIsHolding] = useState(false);
 
-  const endMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    setIsHolding(false);
-  };
+  const [neededSpace, setNeededSpace] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(5);
+  const [maxSlides, setMaxSlides] = useState(0);
 
-  const handleMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const [startX, setStartX] = useState(0);
+
+  const handleMoving = (e: React.MouseEvent<HTMLDivElement>) => {
     if (isHolding) {
-      const oneSlideSpace =
-        sliderWrapRef.current!.clientWidth /
-        sliderWrapRef.current!.childNodes.length;
+      if (startX > e.nativeEvent.offsetX) {
+        const space =
+          startX - e.nativeEvent.offsetX + currentSlide * neededSpace;
 
-      setCurrentSlide(
-        +(((startX - e.nativeEvent.offsetX) / oneSlideSpace) * 1).toFixed(0)
-      );
+        const slides = +(space / neededSpace).toFixed();
+
+        setCurrentSlide(slides);
+      } else {
+        const space = e.nativeEvent.offsetX - startX;
+        const slides = +(space / neededSpace).toFixed();
+
+        setCurrentSlide(maxSlides - slides);
+      }
     }
   };
 
   useEffect(() => {
-    console.log(currentSlide);
-    wrapperRef.current?.scrollTo({
-      left: currentSlide * +sliderWrapRef.current!.clientWidth.toFixed(0),
+    setNeededSpace(
+      wrapperRef.current!.clientWidth / sliderWrapRef.current!.childElementCount
+    );
+
+    setMaxSlides(sliderWrapRef.current!.childElementCount);
+  }, []);
+
+  useEffect(() => {
+    if (currentSlide < 0) {
+      setCurrentSlide(maxSlides);
+    } else if (currentSlide > maxSlides) {
+      setCurrentSlide(0);
+    }
+    wrapperRef.current!.scrollTo({
+      left: currentSlide * wrapperRef.current!.clientWidth,
     });
   }, [currentSlide]);
 
   return (
-    <div ref={wrapperRef} className={cn(st.presenter_360, props.className)}>
-      <div
-        ref={sliderWrapRef}
-        onMouseDown={(e) => startMove(e)}
-        onMouseUp={(e) => endMove(e)}
-        onMouseMove={(e) => handleMove(e)}
-        className={st.presenter_360_wrap__slides}
-      >
-        {props.children}
+    <>
+      <div>needed space: {neededSpace}</div>
+      <div>max: {maxSlides}</div>
+      <div>prev x: {startX}</div>
+      <div>current slide: {currentSlide}</div>
+      <div ref={wrapperRef} className={cn(st.presenter_360, props.className)}>
+        <div className={st.slider_saver}></div>
+        <div ref={sliderWrapRef} className={st.presenter_360_wrap__slides}>
+          {props.children}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
--- src/shared/ui/presenter-360/ui/presenter-slide/presenter-slide.tsx
+++ src/shared/ui/presenter-360/ui/presenter-slide/presenter-slide.tsx
@@ -1,5 +1,3 @@
-import {} from "react";
-
 import st from "../../styles.module.scss";
 import cn from "classnames";
 import { MediaType } from "@/shared/api/internal";
@@ -10,6 +8,7 @@ type PresenterSlide = { media: MediaType; className?: any };
 export const PresenterSlide = (props: PresenterSlide) => {
   return (
     <div className={cn(st.presenter_slide, props.className)}>
+      <div className={st.slider_saver}></div>
       {props.media.image_url ? (
         <Image fill src={props.media.image_url} alt={"alt"} />
       ) : (
